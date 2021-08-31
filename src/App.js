0d86b4335bf26c5ed73289a559bc21a4052f64e0
George Cavazos <cavazosgeorge@yahoo.com>
2021-08-30 20:21:08 -0400

    Completed UI Design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
+
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends Component {
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo input </h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">Todo List</h3>
+        <TodoItem />
+        <button
+          type="button"
+          className="btn btn-block btn-danger text-capitalize mt-5"
+        >
+          Clear List
+        </button>
+      </ul>
+    );
+  }
+}
