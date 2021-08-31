9ba0ca4d08a417d5a9f570c48d40e3d99b594d85
George Cavazos <cavazosgeorge@yahoo.com>
2021-08-30 21:39:04 -0400

    Added TodoItem Functionality | Added TodoList Display Functionality

--- src/App.js
+++ src/App.js
@@ -9,14 +9,58 @@ import uuid from "uuid";
 // this component is going to take care of state, and any change handlers you need to work with your state
 
 class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+
+    console.log(newItem);
+
+    const updatedItems = [...this.state.item, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false,
+    });
+  };
+
+  clearList = () => {
+    this.setState({
+      items: []
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">Todo input </h3>
-            <TodoInput />
-            <TodoList />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList items={this.state.items} 
+            clearList ={this.clearList} />
           </div>
         </div>
       </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,14 +5,18 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
-    return (
-      <ul className="list-group my-5">
-        <h3 className="text-capitalize text-center">Todo List</h3>
-        <TodoItem />
-        <button
-          type="button"
-          className="btn btn-block btn-danger text-capitalize mt-5"
-        >
+      const {items} = this.props;
+        return (
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">Todo List</h3>
+                    {items.map(item => {
+                        return <TodoItem key={item.id} title={item.title} />;
+                    })}               
+
+                <button
+                type="button"
+                className="btn btn-block btn-danger text-capitalize mt-5"
+            >
           Clear List
         </button>
       </ul>
